58fff2f3168c3963c8c52988f4dbab1917f4631f
darren <darren94me@sina.com>
2022-01-21 09:16:01 +0800

    go

--- web/controller/userInfo.go
+++ web/controller/userInfo.go
@@ -21,8 +21,8 @@ var users []User
 
 func init() {
 
-	admin := User{LoginName:"Hanxiaodong", Password:"123456", IsAdmin:"T"}
-	alice := User{LoginName:"ChainDesk", Password:"123456", IsAdmin:"T"}
+	admin := User{LoginName:"lijia", Password:"123456", IsAdmin:"T"}
+	alice := User{LoginName:"darren", Password:"123456", IsAdmin:"T"}
 	bob := User{LoginName:"alice", Password:"123456", IsAdmin:"F"}
 	jack := User{LoginName:"bob", Password:"123456", IsAdmin:"F"}
 
